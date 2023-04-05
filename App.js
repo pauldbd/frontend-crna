c47916b0e7c70082df7d6b0e07c0db3d5a83bfb7
pauldbd <loldeadmemegod@gmail.com>
2023-04-05 00:08:36 -0600

    Init

--- App.js
+++ App.js
@@ -1,11 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
+import { DeviceMotion } from 'expo-sensors';
+import {useState, useEffect} from 'react'
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { DeviceEventEmitter, StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
+
+  let x_list = []; 
+  let y_list = []; 
+  let z_list = []; 
+
+  let [activityStatus, setActivityStatus] = useState("sitting")
+
+  _subscribe = () => {
+    DeviceMotion.addalListener((data) => {
+      let acc = data.acceleration; 
+      x_list.push(acc.x); 
+      y_list.push(acc.y); 
+      z_list.push(acc.z); 
+
+      if (x_list.length == 50){
+        //http://10.0.0.168:5000/
+
+        fetch('http://10.0.0.168:3000/activityrec', {
+          method: 'POST', 
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify([x_list, y_list, z_list]),
+
+        }).then((res) => {
+          res.json().then((data) => {
+            console.log(data['data'])
+            setActivityStatus(data['data'])
+          })
+
+
+        })
+
+
+        x_list = []
+        y_list = []
+        z_list = []
+        
+      }
+
+    })
+    DeviceMotion.setUpdateInterval(15); 
+  }
+
+  _unsubscribe = () => {
+    DeviceMotion.removeAllListeners(); 
+  }
+
+  useEffect(() => {
+    _subscribe(); 
+    return () => {
+      _unsubscribe(); 
+    }
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Extracting Accelerometer Data</Text>
+      <Text>Status: {activityStatus}</Text>
       <StatusBar style="auto" />
     </View>
   );
